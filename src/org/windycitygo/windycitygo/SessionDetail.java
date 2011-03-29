16ba9bfeec3533aa33ab4a6ac1c4984784409859
Kevin McMahon <klmcmahon+github@gmail.com>
2011-03-29 03:20:40 -0500

    changed session list and now include time header

--- src/org/windycitygo/windycitygo/SessionDetail.java
+++ src/org/windycitygo/windycitygo/SessionDetail.java
@@ -1,11 +1,15 @@
 package org.windycitygo.windycitygo;
 
 import org.windycitygo.windycitygo.model.Session;
+import org.windycitygo.windycitygo.util.ImageHelper;
 import org.windycitygo.windycitygo.util.Network;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,7 +19,7 @@ public class SessionDetail extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v(Constants.LOGTAG," " + SessionDetail.CLASSTAG + " onCreate ");
+        Log.v(Constants.LOGTAG," " + SessionDetail.CLASSTAG + " onCreate");
         
         WindyCityGoApplication app = (WindyCityGoApplication) getApplication();
         Session session = app.getCurrentSession();
@@ -30,10 +34,24 @@ public class SessionDetail extends Activity {
         
         titleTextView.setText(session.title);
         presenterTextView.setText(session.speaker.name);
+        if(session.speaker.company != null && session.speaker.company != "")
+        	presenterTextView.append(", "+session.speaker.company);
         descriptionTextView.setText(session.description);
-        speakerBioTextView.setText(session.speaker.bio);
-        if(Network.isNetworkAvailable(this)) {
-        	imgView.setImageBitmap(Network.downloadBitmap(session.speaker.headshot));
+        
+        if(session.speaker.bio != null && session.speaker.bio != "") {
+        	speakerBioTextView.setText(session.speaker.bio);
+        }
+        else {
+        	TextView v = (TextView)findViewById(R.id.bio_header);
+        	v.setVisibility(View.INVISIBLE);
+        	speakerBioTextView.setVisibility(View.INVISIBLE);
+        }
+        
+        if(Network.isNetworkAvailable(this) && session.speaker.headshot != null && session.speaker.headshot != "") {
+        	Bitmap bitmap = Network.downloadBitmap(session.speaker.headshot); 
+        	if(bitmap != null) {
+        		imgView.setImageBitmap(ImageHelper.getRoundedCornerBitmap(bitmap, 12));
+        	}
         }
     }
 }
--- src/org/windycitygo/windycitygo/SessionList.java
+++ src/org/windycitygo/windycitygo/SessionList.java
@@ -4,20 +4,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import javax.crypto.spec.PSource;
+
 import org.windycitygo.windycitygo.model.Session;
 import org.windycitygo.windycitygo.model.SessionCategory;
 
+import com.commonsware.cwac.merge.MergeAdapter;
+
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SessionList extends ListActivity {
 	private static final String CLASSTAG = SessionList.class.getSimpleName();
 	private ArrayList<Session> sessions;
+	private MergeAdapter adapter;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,7 @@ public class SessionList extends ListActivity {
         AssetManager assetManager = getAssets();
         InputStream stream = null;
         ArrayList<SessionCategory> sessionCategories = null;
+        adapter = new MergeAdapter();
         
         try {
             stream = assetManager.open("sessions.xml");
@@ -38,14 +49,33 @@ public class SessionList extends ListActivity {
         	Log.e("SessionsActivity",e.getMessage());
         }
         
-        this.setListAdapter(new SessionAdapter(this, R.layout.session_row, sessions));
+        LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        
+        for (Session s : sessions) {
+        	
+        	View sessionRow = (View) vi.inflate(R.layout.session_row,null);
+        	TextView header = (TextView) sessionRow.findViewById(R.id.session_time_list);
+        	header.setText(s.startTime + " - " + s.endTime);
+        	
+        	TextView top = (TextView) sessionRow.findViewById(R.id.toptext);
+        	TextView bot = (TextView) sessionRow.findViewById(R.id.bottomtext);
+        	top.setText(s.title);
+        	bot.setText(s.speaker.name);
+        	if(s.speaker.company != "")
+        		bot.append(", "+s.speaker.company);
+        	adapter.addView(sessionRow, true); 
+        }
+        
+        this.setListAdapter(adapter);
     } 
     
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        // set the current review to the Application (global state placed there)
+    	Log.v(Constants.LOGTAG, " " + SessionList.CLASSTAG + " onListItemClick");
+    	
+    	// set the current review to the Application (global state placed there)
         WindyCityGoApplication application = (WindyCityGoApplication) getApplication();
-        application.setCurrentSession(this.sessions.get(position));
+        application.setCurrentSession(sessions.get(position));
 
         // startFrom page is not stored in application, for example purposes it's a simple "extra"
         Intent intent = new Intent(Constants.INTENT_ACTION_VIEW_SESSION_DETAIL);
